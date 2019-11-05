4d33e46fbed917991e3e116bb9470184f10c0ec7
klc7778 <kira.clark@sapient.com>
2019-11-05 11:15:48 -0500

    Tic, Tac, Toe Challenge

--- C#-Master-Class/SplatWorld/SplatWorld/Program.cs
+++ C#-Master-Class/SplatWorld/SplatWorld/Program.cs
@@ -4,46 +4,200 @@ namespace SplatWorld
 {
     class MyWorld
     {
+        /*
+        * Tic, Tac, Toe Challenge Game 
+        * Utilizing multidimensional arrays
+        */
+
+        static char[,] board =
+        {
+            {'0','1','2'}, // row 0
+            {'3','4','5'}, // row 1
+            {'6','7','8'}  // row 2
+        };
+
+        static int turns = 0; 
+
         static void Main(string[] args)
         {
-            /*
-             * For loops biotch
-             */
+           int player = 2; // player 1 will start
+           bool inputCorrect = true;
+           int input = 0;
+           //Console.ReadKey();
 
-            int[] nums = new int[10];
 
-            for(int i=0; i < 10; i++)
+            do{
+                
+                if(player == 2)
+                {
+                    player = 1; 
+                    EnterXOrO(player, input); 
+                }
+                else if(player == 1)
+                {
+                    player = 2; 
+                    EnterXOrO(player, input); 
+                }
+
+                SetField();
+
+                #region
+                // Check for a winner
+                chars[] playerChar = {"X", "O"};
+
+                foreach(char playerChar in playerChar)
+                {
+                    if(((board[0,0] == playerChar) && (board[0,1] == playerChar) && (board[0,2] == playerChar))
+                        || ((board[1,0] == playerChar) && (board[1,1] == playerChar) && (board[1,2] == playerChar))
+                        || ((board[2,0] == playerChar) && (board[2,1] == playerChar) && (board[2,2] == playerChar))
+                        || ((board[0,0] == playerChar) && (board[1,0] == playerChar) && (board[2,0] == playerChar))
+                        || ((board[1,1] == playerChar) && (board[0,1] == playerChar) && (board[0,2] == playerChar))
+                        || ((board[0,1] == playerChar) && (board[1,1] == playerChar) && (board[2,1] == playerChar))
+                        || ((board[0,2] == playerChar) && (board[2,1] == playerChar) && (board[2,2] == playerChar))
+                        || ((board[0,0] == playerChar) && (board[1,1] == playerChar) && (board[2,2] == playerChar))
+                        || ((board[1,2] == playerChar) && (board[1,1] == playerChar) && (board[2,0] == playerChar)))
+                    {
+                       if(playerChar == "X")
+                       {
+                            Console.WriteLine("\n Player 2 is the winner!");
+                       }
+                       else
+                        {
+                            Console.WriteLine("\n Player 1 is the winner!");
+                        }
+
+                       // reset the field of play here
+                       Console.WriteLine("Press any key to start the fuck over");
+                       Console.ReadKey(); 
+                       ResetField(); 
+
+                       break; 
+                        
+                    }
+                    else if(turns == 10)
+                    {
+                        Console.WriteLine("Issa tie!");
+                        // reset the field of play here
+                       Console.WriteLine("Press any key to start the fuck over");
+                       Console.ReadKey(); 
+                       ResetField(); 
+
+                       break; 
+                    }
+                }
+
+                #endregion
+
+                #region
+                // test if input is correct 
+                do{
+                    Console.WriteLine("\nPlayer {0}, choose your field: ", player);
+                    try
+                    {
+                         input = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Please enter a number between 1 and 9");
+                    }
+
+                    // check to see if user input is already been selected 
+                    if((input == 1) && (board[0,0] == '1'))
+                        inputCorrect = true; 
+                    else if((input == 2) && (board[0,1] == '2'))
+                        inputCorrect = true;
+                    else if((input == 3) && (board[0,2] == '3'))
+                        inputCorrect = true;
+                    else if((input == 4) && (board[1,0] == '4'))
+                        inputCorrect = true;
+                    else if((input == 5) && (board[1,1] == '5'))
+                        inputCorrect = true;
+                    else if((input == 6) && (board[1,2] == '6'))
+                        inputCorrect = true;
+                    else if((input == 7) && (board[2,0] == '7'))
+                        inputCorrect = true;
+                    else if((input == 8) && (board[2,1] == '8'))
+                        inputCorrect = true;
+                    else if((input == 9) && (board[2,2] == '9'))
+                        inputCorrect = true;
+                    else
+                    {
+                        Console.WriteLine("ENTER SOMETHING ELSE BITCH!"); 
+                        inputCorrect = false; 
+                    }       
+                   
+                }while(!inputCorrect); 
+
+            #endregion
+
+            }while(true); 
+        } 
+
+        public static void ResetField()
+        {
+            char[,] boardInit =
             {
-                nums[i] = i+8;
-            
+                {'0','1','2'}, // row 0
+                {'3','4','5'}, // row 1
+                {'6','7','8'}  // row 2
+            };
+
+            board = boardInit;
+            SetField();
+            turns = 0; 
+        }
+
+        public static void SetField()
+        {
+            Console.Clear(); 
+            Console.WriteLine("|_____|_________|_________|");
+            Console.WriteLine("   {0}|      {1}|      {2}|",board[0,0], board[0,1], board[0,2]);
+            Console.WriteLine("|_____|_________|_________|");
+            Console.WriteLine("   {0}|      {1}|      {2}|",board[0,0], board[0,1], board[0,2]);
+            turns++; 
+
+        }
+
+        public static void EnterXOrO(int player, int input)
+        {
+            char playerSign = " ";
+
+            if(player == 1)
+            {
+                playerSign = "X"; 
+            }
+            else if(player == 2)
+            {
+                playerSign = "O"; 
             }
 
-            for (int j = 0; j < nums.Length; j++)
-            {
-                Console.WriteLine("Element[0] = {1}", j, nums[j]);                
-            }
+            switch(input)
+                {
+                    case 1: board[0,0] = playerSign; 
+                        break;
+                    case 2: board[0,1] = playerSign; 
+                        break;
 
-            /* value of K at position in nums
-             */
-            int counter = 0; 
-            foreach (int k in nums)
-            {
-                Console.WriteLine("Element[0] = {1}", counter, k);
-                counter++;
-            }
-            /* 
-             * create array of 5 names,
-             * and foreach loop to greet all of them */
+                    case 3: board[0,2] = playerSign; 
+                        break;
 
-            string[] friends = new string[]{ "Tommy", "Ghost", "Tasha", "Ja'Net", "Mi'Kael" };
+                    case 4: board[1,0] = playerSign; 
+                        break;
 
-            foreach(string f in friends)
-            {
-                Console.WriteLine("Greetings = {1}!", counter, f);
-                counter++;
-            }
+                    case 5: board[1,1] = playerSign; 
+                        break;
 
-            Console.ReadKey(); 
+                    case 6: board[1,2] = playerSign; 
+                        break;
+                    case 7: board[2,0] = playerSign; 
+                        break;
+
+                    case 8: board[2,1] = playerSign; 
+                        break;
+
+                    case 9: board[2,2] = playerSign; 
+                        break;
+                } // end input switch 
         }
     }
 }
